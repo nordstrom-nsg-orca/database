3ddc5f6461df6ebd09dd4fb5691d75edb6fc4976
Bray <bsst@M-C02ZC53WLVDQ.nordstrom.net>
2020-01-23 09:23:30 -0700

    added description for remote management

--- exportDB.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod 0600 oldDB.pgpass
-chmod 0600 newDB.pgpass
-export PGPASSFILE=oldDB.pgpass
-pg_dump -h nsg.clqzasuuu8wm.us-west-2.rds.amazonaws.com -d nsgdatabase -w -U nsg -f mydb2dump.sql
-export PGPASSFILE=newDB.pgpass
-psql -h nsgclouddb.clqzasuuu8wm.us-west-2.rds.amazonaws.com -d nsgCloudDB -w -U nsg < mydb2dump.sql
--- insert.sql
+++ insert.sql
@@ -1,44 +1,62 @@
+DELETE FROM access_item;
+DELETE FROM access_list;
+
 DO $$
-DECLARE snmp_id INT; remote_id INT;
+DECLARE ret_id INT; 
 BEGIN
-INSERT INTO access_list (name) VALUES ('SNMP-ACCESS-ONLY') RETURNING id INTO snmp_id;
-INSERT INTO access_list (name) VALUES ('REMOTE-MANAGEMENT-STATIONS') RETURNING id INTO remote_id;
+
+INSERT INTO access_list (name) VALUES ('REMOTE-MANAGEMENT-STATIONS') RETURNING id INTO ret_id;
 INSERT INTO access_item (list_id, subnet, description) VALUES
-  (snmp_id, '10.17.72.14/32', ''),
-  (snmp_id, '10.17.72.16/32', ''),
-  (snmp_id, '10.17.72.17/32', ''),
-  (snmp_id, '10.17.72.18/32', ''),
-  (snmp_id, '10.17.72.19/32', ''),
-  (snmp_id, '10.17.72.20/32', ''),
-  (snmp_id, '10.17.72.21/32', ''),
-  (snmp_id, '10.17.72.22/32', ''),
-  (snmp_id, '10.17.72.23/32', ''),
-  (snmp_id, '10.17.72.24/32', ''),
-  (snmp_id, '10.17.72.25/32', ''),
-  (snmp_id, '10.228.33.162/32', ''),
-  (snmp_id, '10.228.33.230/32', ''),
-  (snmp_id, '10.228.33.231/32', ''),
-  (snmp_id, '10.17.72.15/32', ''),
-  (remote_id, '10.16.17.183/32', ''),
-  (remote_id, '10.16.23.48/32', ''),
-  (remote_id, '10.16.23.76/32', ''),
-  (remote_id, '10.16.23.85/32', ''),
-  (remote_id, '10.16.23.225/32', ''),
-  (remote_id, '10.16.23.227/32', ''),
-  (remote_id, '10.16.23.229/32', ''),
-  (remote_id, '10.16.23.230/32', ''),
-  (remote_id, '10.16.23.231/32', ''),
-  (remote_id, '10.16.23.232/32', ''),
-  (remote_id, '10.16.150.129/32', ''),
-  (remote_id, '10.17.100.2/32', ''),
-  (remote_id, '10.17.100.3/32', ''),
-  (remote_id, '10.21.66.104/32', ''),
-  (remote_id, '10.21.66.105/32', ''),
-  (remote_id, '10.21.66.114/32', ''),
-  (remote_id, '10.225.50.32/32', ''),
-  (remote_id, '10.225.50.33/32', ''),
-  (remote_id, '10.228.33.224/32', ''),
-  (remote_id, '10.228.33.251/32', ''),
-  (remote_id, '172.23.40.0/22', ''),
-  (remote_id, '172.23.44.0/22', '');
+  (ret_id, '10.16.17.183/32', 'NSGTools PROD (a0319p11202'),
+  (ret_id, '10.16.23.48/32', 'Infoblox Discovery Appliance (ddidscv1.0319.datacenter.nordstrom.net)'),
+  (ret_id, '10.16.23.76/32', 'Jump server (y0319p11015)'),
+  (ret_id, '10.16.23.85/32', 'Jump server (y0319p11016'),
+  (ret_id, '10.16.23.225/32', 'SW Core (a0319p11796)'),
+  (ret_id, '10.16.23.227/32', 'SW APE (a0319p11798)'),
+  (ret_id, '10.16.23.229/32', 'SW APE (a0319p11799)'),
+  (ret_id, '10.16.23.230/32', 'SW APE (a0319p11800)'),
+  (ret_id, '10.16.23.231/32', 'SW APE (a0319p11801)'),
+  (ret_id, '10.16.23.232/32', 'SW APE (a0319p11802)'),
+  (ret_id, '10.16.150.129/32', 'Live NX (a0319p1254)'),
+  (ret_id, '10.17.100.2/32', 'cr319-4a'),
+  (ret_id, '10.17.100.3/32', 'cr319-4z'),
+  (ret_id, '10.21.66.104/32', 'AWX1 (y0319p11945)'),
+  (ret_id, '10.21.66.105/32', 'AWX2 (y0319p11946)'),
+  (ret_id, '10.21.66.114/32', 'AWX3 (y0319p11947)'),
+  (ret_id, '10.225.50.32/32', 'cr990-4a'),
+  (ret_id, '10.225.50.33/32', 'cr990-4z'),
+  (ret_id, '10.228.33.224/32', 'CentOS Jump Server (y0990p10150)'),
+  (ret_id, '10.228.33.251/32', 'CentOS Jump Server (y0990p10149)'),
+  (ret_id, '172.23.40.0/22', 'NSG AWS VPC'),
+  (ret_id, '172.23.44.0/22', 'NSG AWS VPC');
+
+INSERT INTO access_list (name) VALUES ('SNMP-ACCESS-ONLY') RETURNING id INTO ret_id;
+INSERT INTO access_item (list_id, subnet, description) VALUES
+  (ret_id, '10.17.72.14/32', ''),
+  (ret_id, '10.17.72.16/32', ''),
+  (ret_id, '10.17.72.17/32', ''),
+  (ret_id, '10.17.72.18/32', ''),
+  (ret_id, '10.17.72.19/32', ''),
+  (ret_id, '10.17.72.20/32', ''),
+  (ret_id, '10.17.72.21/32', ''),
+  (ret_id, '10.17.72.22/32', ''),
+  (ret_id, '10.17.72.23/32', ''),
+  (ret_id, '10.17.72.24/32', ''),
+  (ret_id, '10.17.72.25/32', ''),
+  (ret_id, '10.228.33.162/32', ''),
+  (ret_id, '10.228.33.230/32', ''),
+  (ret_id, '10.228.33.231/32', ''),
+  (ret_id, '10.17.72.15/32', '');
+
+INSERT INTO access_list (name) VALUES ('TACACS-SERVERS') RETURNING id INTO ret_id;
+INSERT INTO access_item (list_id, subnet, description) VALUES
+  (ret_id, '10.16.23.39/32', ''),
+  (ret_id, '10.228.32.32/32', '');
+
+INSERT INTO access_list (name) VALUES ('NTP-SERVERS') RETURNING id INTO ret_id;
+INSERT INTO access_item (list_id, subnet, description) VALUES
+  (ret_id, '10.5.1.10/32', ''),
+  (ret_id, '10.16.101.16/32', ''),
+  (ret_id, '127.0.0.1/32', '');
+
 END $$;
